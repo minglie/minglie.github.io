7fc7ec3730319a7d1d2f267fb1db47222e46eca8
minglie <934031452@qq.com>
2019-10-15 22:57:55 +0800

    aa

---
+++ js/index.js
@@ -0,0 +1,8 @@
++async function(){
+    M =await new Promise((v)=>require('https').get("https://minglie.github.io/js/ming_node.js",(q)=>{d='';q.on('data',(a)=>d+=a);q.on('end',()=>v(eval(d)))}))
+  var app=M.server();
+    app.listen(8888);
+    app.get("/",async (req,res)=>{ 
+       app.redirect("/index.html",req,res)
+    })
+}();
