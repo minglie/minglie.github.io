844172eb13cc276487dc14467c25c03f613bcfb3
minglie <934031452@qq.com>
2019-11-02 00:08:34 +0800

    aa

--- sh/ali_ecs.js
+++ sh/ali_ecs.js
@@ -1,22 +1,12 @@
 mkdir /usr/local/node
-wget https://npm.taobao.org/mirrors/node/v6.3.1/node-v6.3.1-linux-x64.tar.gz
-tar zxf node-v6.3.1-linux-x64.tar.gz 
-mv node-v6.3.1-linux-x64 /usr/local/node
+wget  https://nodejs.org/dist/v10.13.0/node-v10.13.0-linux-x64.tar.xz
+tar -xvJf node-v10.13.0-linux-x64.tar.xz
+mv node-v10.13.0-linux-x64 /usr/local/node
 cd /usr/bin
-ln -s /usr/local/node/node-v6.3.1-linux-x64/bin/node node
-ln -s /usr/local/node/node-v6.3.1-linux-x64/bin/npm npm
+ln -s /usr/local/node/node-v10.13.0-linux-x64/bin/node node
+ln -s /usr/local/node/node-v10.13.0-linux-x64/bin/npm npm
 cd ~
-echo "
-var M=require(\"ming_node\");
-var app=M.server();
-app.listen(8888);
-app.get(\"/\",function (req,res){
-   app.redirect(\"/index.html\",req,res)
-})
-app.post(\"/_run_\",function (req,res){
-    eval(req.params.fun)
-    res.send(M.result(\"ok\"))
-})" > index.js
-wget -P ~/static  https://raw.githubusercontent.com/minglie/ming_mockServer0/master/src/static/index.html 
+wget https://raw.githubusercontent.com/minglie/ming_mockServer0/master/index.js 
+wget -P ~/static  https://raw.githubusercontent.com/minglie/ming_mockServer0/master/static/index.html 
 npm i ming_node
 node index.js
--- sh/play_with_docker.js
+++ sh/play_with_docker.js
@@ -1,17 +1,7 @@
-docker pull node
-docker run -ti -p 8888:8888 --name node-1 -v $(pwd):/workspace node bash
+docker pull node:12.6.0-buster-slim
+docker run -ti -p 8888:8888 --name node-1 -v $(pwd):/workspace node:12.6.0-buster-slim bash
 cd ~
-echo "
-var M=require(\"ming_node\");
-var app=M.server();
-app.listen(8888);
-app.get(\"/\",function (req,res){
-   app.redirect(\"/index.html\",req,res)
-})
-app.post(\"/_run_\",function (req,res){
-    eval(req.params.fun)
-    res.send(M.result(\"ok\"))
-})" > index.js
-wget -P ~/static  https://raw.githubusercontent.com/minglie/ming_mockServer0/master/src/static/index.html 
+wget              https://raw.githubusercontent.com/minglie/ming_mockServer0/master/index.js 
+wget -P ~/static  https://raw.githubusercontent.com/minglie/ming_mockServer0/master/static/index.html 
 npm i ming_node
 node index.js
