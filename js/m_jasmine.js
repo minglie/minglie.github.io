601556c95494fcbca2458af94f99e02489648ecb
minglie <934031452@qq.com>
2019-12-28 15:46:03 +0800

    aa

---
+++ js/m_jasmine.js
@@ -0,0 +1,6 @@
+document.write(`
+<link href="https://cdn.bootcss.com/jasmine/3.5.0/jasmine.min.css" rel="stylesheet">
+<script src="https://cdn.bootcss.com/jasmine/3.5.0/jasmine.min.js"></script>
+<script src="https://cdn.bootcss.com/jasmine/3.5.0/jasmine-html.min.js"></script>
+<script src="https://cdn.bootcss.com/jasmine/3.5.0/boot.min.js"></script>
+`)
